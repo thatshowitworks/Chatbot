c04de8faf48930e45a333d50d5ec6362fce06fba
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-08-18 15:48:29 +0530

    Update __main__.py

--- chatbot/__main__.py
+++ chatbot/__main__.py
@@ -7,8 +7,6 @@ if len(sys.argv) not in (1, 3, 4):
     quit(1)
 else:
     app.start()
-    LOGGER.info("Simple chatbot written using the pyrogram library.\n " \
-    "Uses Intellivoid's Coffeehouse API.\n" \
-    "Written by @TheRealPhoenix on Telegram.")
+    LOGGER.info("Simple chatbot written using the pyrogram library.\nUses Intellivoid's Coffeehouse API.\n")
     LOGGER.info("Your bot is now online. Check .help for help!")
     app.idle()
